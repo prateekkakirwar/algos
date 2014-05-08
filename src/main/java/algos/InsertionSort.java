0e2de88290f9809aa605ce80632fd5bb5dc696f4
Prateek Kakirwar <prateekkakirwar@gmail.com>
2014-05-07 22:11:30 -0700

    sample insertion sort

--- src/main/java/algos/InsertionSort.java
+++ src/main/java/algos/InsertionSort.java
@@ -3,6 +3,30 @@ package algos;
 public class InsertionSort
 {
 	public static void main(String[] args) {
-	System.out.println("Hello Prateek");
+	//initial array 
+	int a[] = {8,2,4,9,3,6};
+	int i,j,key;
+	System.out.println("Initial Arrays");
+	for(int k = 1; k<a.length;k++)
+		System.out.print(a[k] + " ");
+	
+	for(j=1; j<a.length;j++)
+	{
+		key=a[j];
+		i=j-1;
+		while(i>0 & a[i]>key)
+		{
+			a[i+1]=a[i];
+			i--;
+			
+		}
+		a[i+1]=key;
+	}
+	
+	System.out.println();
+	System.out.println("Sorted Arrays");
+	for(int k = 1; k<a.length; k++)
+		System.out.print(a[k] + " ");
+	
 	}	
 }
