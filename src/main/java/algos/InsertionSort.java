9bedc477f534f7dd5e5d6469b8e95c6c3bd8855a
Prateek Kakirwar <prateekkakirwar@gmail.com>
2014-05-07 23:11:24 -0700

    Merge Sort

--- src/main/java/algos/InsertionSort.java
+++ src/main/java/algos/InsertionSort.java
@@ -4,29 +4,27 @@ public class InsertionSort
 {
 	public static void main(String[] args) {
 	//initial array 
-	int a[] = {8,2,4,9,3,6};
+	int a[] = {9,1,24,5,2};
 	int i,j,key;
 	System.out.println("Initial Arrays");
-	for(int k = 1; k<a.length;k++)
+	for(int k = 0; k<a.length;k++)
 		System.out.print(a[k] + " ");
 	
 	for(j=1; j<a.length;j++)
 	{
 		key=a[j];
 		i=j-1;
-		while(i>0 & a[i]>key)
+		while(i>=0 && a[i]>key)
 		{
 			a[i+1]=a[i];
 			i--;
 			
 		}
 		a[i+1]=key;
+		System.out.println();
+		for(int k = 0; k<a.length; k++)
+			System.out.print(a[k] + " ");
 	}
-	
-	System.out.println();
-	System.out.println("Sorted Arrays");
-	for(int k = 1; k<a.length; k++)
-		System.out.print(a[k] + " ");
 	
 	}	
 }
---
+++ src/main/java/algos/MergeSort.java
@@ -0,0 +1,10 @@
+package algos;
+
+public class MergeSort
+{
+	public static void main(String args[])
+	{
+		System.out.println("Merge Sort");
+	}
+	
+}
